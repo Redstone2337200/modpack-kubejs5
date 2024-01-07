ba88204f57d5db6327333c4e00e4400c08bb16e3
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-01-07 12:43:31 +0800

    Add files via upload

---
+++ kubejs/server_scripts/loots/blocks.js
@@ -0,0 +1,9 @@
+onEvent('lootjs', event => {
+    event.addBlockLootModifier('kubejs:ordin_gold_block').addLoot('kubejs:ordin_gold_block')
+    event.addBlockLootModifier('kubejs:great_gold_block').addLoot('kubejs:great_gold_block')
+    event.addBlockLootModifier('kubejs:ultra_gold_block').addLoot('kubejs:ultra_gold_block')
+    event.addBlockLootModifier('kubejs:legend_gold_block').addLoot('kubejs:legend_gold_block')
+    event.addBlockLootModifier('kubejs:myth_gold_block').addLoot('kubejs:myth_gold_block')
+    event.addBlockLootModifier('kubejs:huohui_block').addLoot('kubejs:huohui_block')
+    event.addBlockLootModifier('kubejs:huorong_block').addLoot('kubejs:huorong_block')
+})
